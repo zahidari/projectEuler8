85ffe46dac962da8361e504b0d9dc8b59abe0566
zahidari <mzhdar1@gmail.com>
2023-09-10 12:20:13 +0200

    added a main loop ect. Finished

--- src/projecteuler8/ProjectEuler8.java
+++ src/projecteuler8/ProjectEuler8.java
@@ -1,6 +1,6 @@
 
 package projecteuler8;
-
+import java.util.*;
 /**
  *
  * @author github.com/zahidari
@@ -9,36 +9,59 @@ public class ProjectEuler8 {
 
     
     
-   static  void  func(int searchedAdjacentNumLength ,String str[]){
+   static  void  func(int theLengthOfSearchedAdjacentNumber ,char ch[]){
+       
        
        int index = 0; // to iterate
        int temp1 = 1; // multiplySum every time the set
-       int theIndex = 0; // index of the result beginning index
+       int searchedIndex = 0; // index of the result beginning index
+       
        
        //compare and find the highest
-       int tempMultipyedSum = 1;
-       int tempHighestSum = 1;
-       
-       
-       
+       long tempProduct = 1;
+       long tempHighestProduct = 1;
 
-       for(int i = 0; i < searchedAdjacentNumLength; i ++){
+
+       
+       while(true)
+       {
            
-           // if reached at the end it will end the loop
-           if(searchedAdjacentNumLength + index > str.length)
-              break;
-           
-          // calculate the multiply of the adjacent numbers
-          temp1 = Integer.parseInt(str[index+i]);
-          tempMultipyedSum *= temp1;
-          
-          
-          // compare the highest and replace
-          if(tempHighestSum < tempMultipyedSum){
-           tempHighestSum = tempMultipyedSum;
-           theIndex = index;
+            // if reached at the end it will end the loop
+            if(theLengthOfSearchedAdjacentNumber + index > ch.length)
+                break; 
+            
+            
+            /*if( '0'  == ch[index + 12] ){
+                index += 13;
+                continue;
+            }*/
+
+            for(int i = 0; i < theLengthOfSearchedAdjacentNumber; i ++)
+            {
+                
+                // calculate the multiply of the adjacent numbers
+                
+                temp1 = Character.getNumericValue(ch[index+i]);
+                tempProduct *= temp1; // sum = sum * temp ;
+
+            }
+            
+            System.out.print( index+"  ");
+            System.out.print( tempHighestProduct);
+            System.out.print("   " + tempProduct + "\n");
+            
+            // compare the highest and replace
+            if(tempHighestProduct < tempProduct){
+                tempHighestProduct = tempProduct;
+                searchedIndex = index;
+            }
+       
+            tempProduct = 1;
+            index++;
        }
-       }
+       System.out.println("The Highest Product is:"+tempHighestProduct);
+       System.out.println("the searched index is:"+searchedIndex);
+       
        
    }
    
@@ -67,8 +90,10 @@ public class ProjectEuler8 {
                 + "05886116467109405077541002256983155200055935729725"
                 + "71636269561882670428252483600823257530420752963450";
 
+         char[] ch = the1000digitNum.toCharArray();
         
-    
+        func(13,ch );
+        
     }
     
 }
